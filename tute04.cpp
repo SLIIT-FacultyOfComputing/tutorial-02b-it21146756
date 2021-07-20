bf2644a762bbbb1545a70a499db63161f19be4c4
it21146756 <87510932+it21146756@users.noreply.github.com>
2021-07-20 07:37:12 +0000

    I have completed 4 question

--- tute04.cpp
+++ tute04.cpp
@@ -22,8 +22,23 @@ int main() {
   std::cin >> n;
   std::cout << "Enter a value for r ";
   std::cin >> r;
+
+  
   std::cout << "nCr = ";
-  std::cout << nCr(n,r);
+  std::cout <<Factorial(n);
   std::cout << std::endl;
   return 0;
 }
+long nCr(int n,int r){
+  long nCr = Factorial(n);///Factorial(r)*Factorial(n-r);
+
+  return nCr;
+}
+long Factorial(int no){
+long fac = 1;
+    for (int r=no; r >= 1; r--) {
+        fac = fac * r;
+    } 
+    return fac;
+}
+
